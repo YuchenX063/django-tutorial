3aeab51950da3b4bd9fd3ccfda6e4e14f21f51ff
Yuchen Xiong <xiongyuc@bc.edu>
2025-01-07 15:10:25 -0500

    01/07

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3 # by default, provide enough fields for 3 choices
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+# create a model admin class and pass it to admin.site.register() as the second argument
+# inlines -- allow users to add question-related choices on the same page; used for models with foreign key relationships
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,21 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
+app_name = "polls"
+
+'''urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.vote, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]'''
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+# DetailView expects the primary key value captured from the URL to be called "pk"
+# class-based views have an as_view() class method
+# advantages of class-based views: can be reused and extended by other views, can be customized by overriding attributes and methods
